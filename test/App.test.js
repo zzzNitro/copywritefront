299ce3cbe7b2ca400b7237b3a8c90c63471d435d
Nitro <pablo.benito@rocketmail.com>
2022-06-08 00:49:38 -0400

    added jest to test initialState value.

---
+++ test/App.test.js
@@ -0,0 +1,10 @@
+const initialState = {
+    texts: [],
+ }
+
+describe('CopyWrite App', function() {
+    it('should have an initialState that contains an empty array called texts', ()=>{
+        expect(initialState.texts).toEqual([]);
+    } );
+});    
+
